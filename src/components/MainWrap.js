d23fa52c4f8b95e1932f59d43e8a9b060800f2f3
savadkuhi <mohammadsavadkohi@johnsavad.local>
2020-06-20 20:36:51 +0430

    add MainWrap compnent

--- src/components/MainWrap.js
+++ src/components/MainWrap.js
@@ -1,17 +1,16 @@
-import React , {useEffect} from "react"
-import { setLanguage} from "../state_manager/actions" 
-import {connect} from "react-redux";
-import {IntlProvider} from "react-intl";
-import {languages} from "../translations";
-import {HashRouter as Router } from "react-router-dom";
+import React  from "react"
+// import { setLanguage} from "../state_manager/actions" 
+// import {useSelector , useDispatch} from "react-redux";
+// import {IntlProvider} from "react-intl";
+// import {languages} from "../translations";
+// import {HashRouter as Router } from "react-router-dom";
 
 
 function MainWrap(){
 
-    useEffect(() => console.log('mounted'), []);
 
-
+    return <></>
 
 }
 
-export default connect(null , null )(MainWrap) 
+export default MainWrap 
